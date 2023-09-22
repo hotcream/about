e4ebeb3973fe0d685e2ce5b958a0e1b9cf7fff08
Zinov Nikita <zinov.nikita2010@yandex.ru>
2023-09-23 03:23:26 +0500

    first commit

--- src/app2.vue
+++ src/app2.vue
@@ -1,44 +1,100 @@
 <template>
-  <a-layout-content :style="{ margin: '50px auto', padding: '24px', minHeight: '280px', maxWidth: '1280px' }">
-    <h1>Зинов Никита Александрович</h1>
-    <h3>Fullstack разработчик</h3>
-    <p id="about">
-      Начиная с 2014 года я занимаюсь разработкой и сопровождением веб-приложений в банковской сфере.
-      Сначалао это было приложение на IIS (VBScript). Потом приложение на Apache Tomcat, которое трансформировалось в большое приложение Node.js + Webpack + Vue.js.
-      Которое работает в Kubernetes кластере, а разработка в Docker.
-    </p>
-    <a-divider />
-    <h2 id="experience">Опыт работы {{ Math.floor(startWork) }} лет и {{ Math.ceil(startWork % 1 * 12) }} мес.</h2>
-    <a-timeline id="experience" mode="alternate">
-      <a-timeline-item v-for="(e,i) in experience" :color="e.type === 'job' ? 'blue' : 'green'">
-          <template v-if="i == 0" #dot><ClockCircleOutlined style="font-size: 16px" /></template>
-          <h3>{{e.сompany}}. {{e.title}}</h3>
-          <small>{{ e.type === 'job' ? 'Работа' : 'Обучение' }} {{e.period}}</small>
-          <ul v-if="e.responsibilities && e.responsibilities.length > 0">
-            <li v-for="rb in e.responsibilities">{{rb}}</li>
-          </ul>
-          <h4 v-if="e.results && e.results.length > 0">Результаты работы:</h4>
-          <ul v-if="e.results && e.results.length > 0">
-            <li v-for="rs in e.results">{{rs}}</li>
-          </ul>
+  <a-result class="header" :title="config.name" :sub-title="config.jobtitle">
+    <template #icon>
+      <a-avatar src="/photo.jpg" :size="{ xs: 100, sm: 150, md: 200, lg: 250, xl: 300, xxl: 350 }"/>
+    </template>
+    <a-card class="desc">{{config.description}}</a-card>
+    <icon-font type="icon-twitter" />
+    <CaretDownOutlined class="down-arrow"/>
+  </a-result>
+  <a-layout-content v-if="config" class="content">
+    <a-divider>Опыт работы {{ Math.floor(startWork) }} лет и {{ Math.ceil(startWork % 1 * 12) }} мес.</a-divider>
+    <a-timeline id="experience" :mode="windowWidth < 1200 ? 'left' : 'alternate'">
+      <a-timeline-item v-for="(e,i) in config.experience" :color="e.type === 'job' ? 'blue' : 'green'">
+        <template v-if="i == 0" #dot><ClockCircleOutlined style="font-size: 16px" /></template>
+        <a-badge-ribbon :text="(e.type === 'job' ? 'Работа ' : 'Обучение ') + e.period"
+          :color="e.type === 'job' ? 'primary' : 'green'"
+          :placement="windowWidth < 1200 || e.type === 'job' ? 'end' : 'start'">
+          <a-card :title="e.сompany+'. '+e.title" size="small" :bodyStyle="{textAlign: 'left'}">
+            <ul v-if="e.responsibilities && e.responsibilities.length > 0">
+              <li v-for="rb in e.responsibilities">{{rb}}</li>
+              <template v-if="e.links && e.links.length > 0" v-for="l in e.links" >
+                <li><a :href="l.to" target="_blank"><LinkOutlined /> {{ l.text }}</a></li>
+              </template>
+            </ul>
+            <a-divider v-if="e.results && e.results.length > 0" orientation="left" orientation-margin="0px">Результат работы</a-divider>
+            <ul v-if="e.results && e.results.length > 0">
+              <li v-for="rs in e.results">{{rs}}</li>
+            </ul>
+            <a-divider orientation="left" orientation-margin="0px">Ключевые навыки</a-divider>
+            <a-tag v-if="e.tags && e.tags.length > 0" color="blue" v-for="t in e.tags">{{t}}</a-tag>
+          </a-card>
+        </a-badge-ribbon>
       </a-timeline-item>
     </a-timeline>
-    <a-float-button-group shape="square" :style="{ right: '24px' }">
-      <a-float-button description="hh"></a-float-button>
-      <a-float-button description="vk"></a-float-button>
-      <a-back-top :visibility-height="0" />
-    </a-float-button-group>
+    <a-divider>Мои увлечения</a-divider>
+    <a-divider>Мои контакты</a-divider>
+    <div style="text-align: center;">
+      <template v-if="config.contacts && config.contacts.length > 0" v-for="c in config.contacts" >
+        <a-popover :overlay-inner-style="{ padding: 0 }">
+          <template #content>
+            <a-qrcode :value="c.link" :icon="c.icon" :bordered="false" />
+          </template>
+          <a-button type="link" :href="c.link" target="_blank" style="margin:20px">
+            <img style="height:32px;margin-right: 10px;" :src="c.icon" :alt="c.title" :title="c.title"/>
+            {{ c.title }}
+          </a-button>
+        </a-popover>
+      </template>
+    </div>
   </a-layout-content>
 </template>
 <script lang="ts" setup>
 import { ref } from 'vue'
-import { ClockCircleOutlined } from '@ant-design/icons-vue'
-import config, { ExperienceType } from './config'
+import { ClockCircleOutlined, LinkOutlined, CaretDownOutlined, GithubOutlined } from '@ant-design/icons-vue'
 
+import config from './config'
+document.title = `${config.name} | ${config.jobtitle}`
 const startWork = ref((Date.now() - config.start.getTime()) / 1000 / 60 / 60 / 24 / 365.25 )
-
-const experience = ref<ExperienceType[]>(config.experience)
+const windowWidth = ref(window.innerWidth)
+window.addEventListener('resize',() => {
+  windowWidth.value = window.innerWidth
+})
 </script>
-<style scoped>
+<style scoped lang="scss">
+.header {
+  height: 80vh;
+  max-height: 800px;
+  position: relative;
+  background: url(/bg.jpg);
+  background-size: cover;
+  background-position: 50% 50%;
+  background-attachment: fixed;
+  :deep(>*) {
+    color: #fff;
+  }
+  .down-arrow {
+    position: absolute;
+    bottom: 1vh;
+    left: 48.5vw;
+    font-size: 3vh;
+    color: #fff;
+    animation: look-down 2s ease-out infinite;
+  }
+  .desc {
+    margin: 0 auto;
+    max-width: 1280px;
+    color: #000;
+  }
+}
+.content {
+  margin: 0 auto;
+  padding: 74px 24px;
+  min-height: 280px;
+  max-width: 1280px;
+  :deep(.ant-timeline-item-head-custom) {
+    background: none;
+  }
+}
 </style>
   
--- src/config.ts
+++ src/config.ts
@@ -5,30 +5,58 @@ export type ExperienceType = {
   title: string
   responsibilities?: string[]
   results?: string[]
+  tags?: string[]
+  links?: {
+    to:string,
+    text: string
+  }[]
+}
+export type ContactType = {
+  title: string
+  icon: string
+  link: string
 }
 export type ConfigType = {
-  experience: ExperienceType[],
   start: Date
+  name: string
+  jobtitle: string
+  description: string
+  experience: ExperienceType[]
+  contacts: ContactType[]
 }
 export default {
   start: new Date(2014,5,1),
+  name: 'Зинов Никита Александрович',
+  jobtitle: 'Full Stack разработчик',
+  description: 'Начиная с 2014 года я занимаюсь разработкой и сопровождением веб-приложений в банковской сфере.',
   experience: [
     {
       "type": "job",
-      "period": "2023 - по настоящее время",
-      "сompany": "ООО «Интеллектуальные программные решения»",
+      "period": "2023 - по н.в.",
+      "сompany": "Интеллектуальные программные решения",
       "title": "Главный разработчик",
       "responsibilities": [
         "Frontend + Backend + DevOps",
         "Поддержка и доработка реализованных ранее для Банка Уралсиб систем",
         "Доработка и сопровождение Производственной системы, в которой ведется работа разработчиков, учет трудозатрат, сметы для взаиморасчетов с заказчиками"
-      ]
+      ],
+      "tags": ["Vuejs","Ruby on Rails","PostgreSQL", "Ant Design", "Kubernetes"],
+      "links": [{to: 'https://www.smartsoftware.ru/', text: 'Smart Software Solutions'}]
     },
     {
       "type": "learn",
       "period": "2022 - 2023",
       "сompany": "Яндекс Практикум",
-      "title": "Мидл фронтенд-разработчик"
+      "title": "Мидл фронтенд-разработчик",
+      "responsibilities": [
+        "Проект «Веб-мессенджер»",
+        "Командный проект - выполнял роль лидера команды",
+        "Алгоритмы и структуры данных"
+      ],
+      "tags": ["TypeScript","Webpack","Parcel","React","Web API","Canvas API","CI / CD","Yandex Cloud",
+        "React-router","Nginx","WebSocket","PostgreSQL","Server Side Rendering","Docker","Jest"],
+        "links": [{to: 'https://practicum.yandex.ru/middle-frontend/', text: 'Описание курса'}]
+        
     },
     {
       "type": "job",
@@ -47,13 +75,23 @@ export default {
         "Масштабирование с портала оформления заявок Service Desk, до полноценного портала ITSM",
         "Внедрение механизма ведения проектов ИТ - работ, трудозатрат, смет, планирование итд",
         "Создание инструмента описания практически любого бизнес-процесса банка"
-      ]
+      ],
+      "tags": ["Vue JS","Ruby on Rails","Oracle", "My SQL", "Bootstrap Vue", "Gitlab", "Docker"],
+      "links": [{to: 'https://www.uralsib.ru/', text: 'Банк УРАЛСИБ'}]
     },
     {
       "type": "learn",
       "period": "2014 - 2018",
-      "сompany": "Уфимский государственный авиационный технический университет",
-      "title": "Факультет информатики и робототехники"
+      "сompany": "УГАТУ",
+      "title": "Факультет информатики и робототехники",
+      "responsibilities":[
+        "Анализ бизнес-процессов",
+        "Разработка проектов автоматизации процессов",
+        "Создание информационных систем в прикладных областях",
+        "Управление проектами информатизации"
+      ],
+      "tags": ["09.03.03 Прикладная информатика", "Анализ данных", "C#", "Java", "ООП", "Базы данных"],
+      "links": [{to: 'https://ugatu.su/', text: 'УГАТУ'}]
     },
     {
       "type": "job",
@@ -71,7 +109,26 @@ export default {
         "Реализована возможность формирования отчетности по данным системы",
         "Сформированы организационные правила по контролю версий",
         "Настроено информирование администраторов о возникших ошибках"
-      ]
+      ],
+      "tags": ["IIS","VBScript","MS SQL","JavaScript"],
+      "links": [{to: 'https://www.uralsib.ru/', text: 'Банк УРАЛСИБ'}]
     }
-  ]
+  ],
+  contacts: [{
+    title: '@ZinovNikita',
+    icon: '/github.png',
+    link: 'https://github.com/ZinovNikita'
+  },{
+    title: '@ZinNikAlex',
+    icon: '/vk.png',
+    link: 'https://vk.com/zinnikalex'
+  },{
+    title: 'Никита Зинов',
+    icon: '/hh.png',
+    link: 'https://ufa.hh.ru/resume/5498def9ff083d236c0039ed1f6d33314f7a75'
+  },{
+    title: '@ZinovNA',
+    icon: '/tg.png',
+    link: 'https://t.me/ZinovNA'
+  }]
 } as ConfigType
